25f8ad20674e253437d10aef7600b8d819beeb84
prathyu99 <prathyu13579@gmail.com>
2024-04-23 23:58:55 -0400

    added comments to the code studenttasks

--- src/pages/StudentTasks/StudentTasks.tsx
+++ src/pages/StudentTasks/StudentTasks.tsx
@@ -7,8 +7,6 @@ import styles from './StudentTasks.module.css';
 import StudentTasksBox from './StudentTasksBox';
 import testData from './testData.json';
 
-
-
 // Define the types for a single task and the associated course
 type Task = {
   id: number;
@@ -17,31 +15,31 @@ type Task = {
   topic: string;
   currentStage: string;
   reviewGrade: string | { comment: string };
-  badges: string | boolean; // Assuming badges can be either a string or a boolean
+  badges: string | boolean;
   stageDeadline: string;
   publishingRights: boolean;
 };
 
+// Empty props for FC as no props are needed currently
+type Props = {};
 
-type Props = {
-};
-
-
+// Main functional component for Student Tasks
 const StudentTasks: React.FC = () => {
+  // State and hooks initialization
   const participantTasks = testData.participantTasks;
   const currentUserId = testData.current_user_id;
   const auth = useSelector((state: RootState) => state.authentication);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  // State to hold tasks
   const [tasks, setTasks] = useState<Task[]>([]);
   const exampleDuties = testData.dueTasks;
   const taskRevisions = testData.revisions;
   const studentsTeamedWith = testData.studentsTeamedWith;
 
-
+  // Effect to process tasks on component mount or update
   useEffect(() => {
-
     if (participantTasks) {
       const filteredParticipantTasks = participantTasks.filter(task => task.participant_id === currentUserId);
 
@@ -60,85 +58,83 @@ const StudentTasks: React.FC = () => {
       });
       setTasks(mergedTasks);
     }
-}, [participantTasks]);
+  }, [participantTasks]);
 
-  // Function to toggle publishing rights
+  // Callback to toggle publishing rights
   const togglePublishingRights = useCallback((id: number) => {
     setTasks(prevTasks => prevTasks.map(task =>
       task.id === id ? {...task, publishingRights: !task.publishingRights} : task
     ));
   }, []);
 
-  // Render the list of tasks within a container
+  // Component render method
   return (
     <div className="assignments-page">
-
-    <h1 className="assignments-title">Assignments</h1>
-
-    <div className={styles.pageLayout}>
-
-    <aside className={styles.sidebar}>
-        <StudentTasksBox  dueTasks={exampleDuties}
+      <h1 className="assignments-title">Assignments</h1>
+      <div className={styles.pageLayout}>
+        <aside className={styles.sidebar}>
+          <StudentTasksBox
+            dueTasks={exampleDuties}
             revisions={taskRevisions}
             studentsTeamedWith={studentsTeamedWith}
-        />
-    </aside>
-    <div className={styles.mainContent}>
-
-        <table className={styles.tasksTable}>
-        <thead>
-            <tr>
-              <th>Assignment</th>
-              <th>Course</th>
-              <th>Topic</th>
-              <th>Current Stage</th>
-              <th>Review Grade</th>
-              <th>Badges</th>
-              <th>
-                Stage Deadline
-                <img src="assets/icons/info.png" alt="Info" title="You can change 'Preferred Time Zone' in 'Profile' in the banner." />
-              </th>
-              <th>
-                Publishing Rights
-                <img src="assets/icons/info.png" alt="Info" title="Grant publishing rights" />
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task) => (
-              <tr key={task.id}>
-                <td><Link to={`/student_task_detail/${task.id}`}>{task.assignment}</Link></td>
-                <td>{task.course}</td>
-                <td>{task.topic}</td>
-                <td>{task.currentStage}</td>
-                <td>
-                  {task.reviewGrade === "N/A" ? "NA" :
-                    <img src="assets/icons/info.png" alt="Review Grade" title={(task.reviewGrade as any).comment || ''} />
-                  }
-                </td>
-                <td>{task.badges}</td>
-                <td>{task.stageDeadline}</td>
-                <td className={styles.centerCheckbox}>
-                  <input
-                    type="checkbox"
-                    checked={task.publishingRights}
-                    onChange={() => togglePublishingRights(task.id)}
-                  />
-                </td>
+          />
+        </aside>
+        <div className={styles.mainContent}>
+          <table className={styles.tasksTable}>
+            <thead>
+              <tr>
+                <th>Assignment</th>
+                <th>Course</th>
+                <th>Topic</th>
+                <th>Current Stage</th>
+                <th>Review Grade</th>
+                <th>Badges</th>
+                <th>
+                  Stage Deadline
+                  <img src="assets/icons/info.png" alt="Info" title="You can change 'Preferred Time Zone' in 'Profile' in the banner." />
+                </th>
+                <th>
+                  Publishing Rights
+                  <img src="assets/icons/info.png" alt="Info" title="Grant publishing rights" />
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-    </div>
-    </div>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task.id}>
+                  <td><Link to={`/student_task_detail/${task.id}`}>{task.assignment}</Link></td>
+                  <td>{task.course}</td>
+                  <td>{task.topic}</td>
+                  <td>{task.currentStage}</td>
+                  <td>
+                    {task.reviewGrade === "N/A" ? "NA" :
+                      <img src="assets/icons/info.png" alt="Review Grade" title={(task.reviewGrade as any).comment || ''} />
+                    }
+                  </td>
+                  <td>{task.badges}</td>
+                  <td>{task.stageDeadline}</td>
+                  <td className={styles.centerCheckbox}>
+                    <input
+                      type="checkbox"
+                      checked={task.publishingRights}
+                      onChange={() => togglePublishingRights(task.id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
 
-    {/* Footer Section Added */}
-          <div className={styles.footer}>
-            <Link to="https://wiki.expertiza.ncsu.edu/index.php/Expertiza_documentation" className={styles.footerLink}>Help</Link>
-            <Link to="https://research.csc.ncsu.edu/efg/expertiza/papers" className={styles.footerLink}>Papers on Expertiza</Link>
-          </div>
+      {/* Footer section */}
+      <div className={styles.footer}>
+        <Link to="https://wiki.expertiza.ncsu.edu/index.php/Expertiza_documentation" className={styles.footerLink}>Help</Link>
+        <Link to="https://research.csc.ncsu.edu/efg/expertiza/papers" className={styles.footerLink}>Papers on Expertiza</Link>
+      </div>
     </div>
   );
 };
 
+// Export the component for use in other parts of the application
 export default StudentTasks;
